f6664adaa77317b390819cb233fdcaa5d4363b36
Via <1627973075@qq.com>
2018-06-12 18:01:03 +0800

    commit FizzBuzzGame

--- src/main/java/com/thoughtworks/FizzBuzzGame.java
+++ src/main/java/com/thoughtworks/FizzBuzzGame.java
@@ -1,6 +1,7 @@
 package com.thoughtworks;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -8,11 +9,32 @@ public class FizzBuzzGame {
     private List<String> results = new ArrayList<>();
 
     public void start(int count) {
-
+        for (int i = 1; i <= count; i++){
+            String result = judge(i);
+            results.add(result);
+        }
     }
 
     public List<String> getResults() {
         return results;
+    }
+
+    public String judge(int i) {
+        String result = "";
+        if (3 == (i % 10) || 3 == (i / 10)) {
+            result = "Fizz";
+        }else{
+            if (3 == i || 0 == (i%3))
+                result = result + "Fizz";
+            if (0 == (i%5))
+                result = result + "Buzz";
+            if (7 == i || 0 == (i%7))
+                result = result + "Whizz";
+        }
+
+        if (result == "")
+            result = String.valueOf(i);
+        return result;
     }
 }
 
--- src/test/java/com/thoughtworks/FizzBuzzGameTest.java
+++ src/test/java/com/thoughtworks/FizzBuzzGameTest.java
@@ -2,9 +2,69 @@ package com.thoughtworks;
 
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.*;
+
 public class FizzBuzzGameTest {
     @Test
     public void should_return_Fizz_when_have_3() {
+        FizzBuzzGame fbg = new FizzBuzzGame();
+        String result = "Fizz";
+        assertThat(fbg.judge(3), equalTo(result));
+    }
 
+    @Test
+    public void shoul_return_Buzz_when_have_5(){
+        FizzBuzzGame fbg = new FizzBuzzGame();
+        String result = "Buzz";
+        assertThat(fbg.judge(5), equalTo(result));
+    }
+
+    @Test
+    public void should_return_Whizz_when_have_7(){
+        FizzBuzzGame fbg = new FizzBuzzGame();
+        String result = "Whizz";
+        assertThat(fbg.judge(7), equalTo(result));
+    }
+
+    @Test
+    public void should_return_Fizz_when_have_9(){
+        FizzBuzzGame fbg = new FizzBuzzGame();
+        String result = "Fizz";
+        assertThat(fbg.judge(9), equalTo(result));
+    }
+
+    @Test
+    public void should_return_Buzz_when_have_10(){
+        FizzBuzzGame fbg = new FizzBuzzGame();
+        String result = "Buzz";
+        assertThat(fbg.judge(10), equalTo(result));
+    }
+    @Test
+    public void should_return_Whizz_when_have_14(){
+        FizzBuzzGame fbg = new FizzBuzzGame();
+        String result = "Whizz";
+        assertThat(fbg.judge(14), equalTo(result));
+    }
+
+    @Test
+    public void should_return_FizzWhizz_when_have_21(){
+        FizzBuzzGame fbg = new FizzBuzzGame();
+        String result = "FizzWhizz";
+        assertThat(fbg.judge(21), equalTo(result));
+    }
+
+    @Test
+    public void should_return_Fizz_when_have_13(){
+        FizzBuzzGame fbg = new FizzBuzzGame();
+        String result = "Fizz";
+        assertThat(fbg.judge(13), equalTo(result));
+    }
+
+    @Test
+    public void should_return_Fizz_when_have_35(){
+        FizzBuzzGame fbg = new FizzBuzzGame();
+        String result = "Fizz";
+        assertThat(fbg.judge(35), equalTo(result));
     }
 }
